3a84616da53c5a12754b8cd45a9d01b0f9a9f31a
Lauri Leukkunen <lleukkun@leka.rahina.org>
2006-08-09 01:54:03 +0300

    work on the lua rules system

--- redir_scripts/parts/sample.lua
+++ redir_scripts/parts/sample.lua
@@ -1,2 +1,50 @@
---print "hello from sample.lua!\n"
+-- Copyright (C) 2006 Lauri Leukkunen <lle@rahina.org>
+-- Licensed under so called MIT license.
+
+-- print "hello from sample.lua!\n"
+
+-- All these values are treated as Lua patterns, 
+-- except the map_to and custom_map_func fields.
+-- In map_to these have special meaning:
+--
+-- "="			map to tools_root .. "/" .. path
+-- "=/some/path"	map to tools_root .. "/some/path" .. "/" .. path
+-- nil			no mapping, use straight
+--
+-- Any other value is prepended to path (map_to .. "/" .. path).
+-- 
+-- The rules are exported from this file to the main.lua engine
+-- by putting them all into export_rules table variable.
+-- They will be evaluated in the order they are listed there.
+-- Also the files matching parts/*.lua are sorted alphabetically
+-- and used in that order.
+--
+-- custom_map_func allows you to specify your own path mapping logic.
+-- The function takes these parameters: 
+-- function(binary_name, func_name, work_dir, path, rule) 
+-- and is expected to return the mapped path. rule argument contains
+-- the rule which triggered the function invocation.
+
+sample_rule1 = {
+	binary = "python",
+	func_name = "exec",
+	func_param = nil,
+	path = "^/usr/bin",
+	map_to = "=/usr/local",
+	custom_map_func = nil
+}
+
+sample_rule2 = {
+	binary = "*",
+	func_name = "exec",
+	func_param = nil,
+	path = "^/usr/bin",
+	map_to = "=",
+	custom_map_func = nil
+}
+
+export_rules = {
+	sample_rule1,
+	sample_rule2
+}
 

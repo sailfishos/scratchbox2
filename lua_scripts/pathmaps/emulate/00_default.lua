6fa24730bbc56a229e759a670669bfcc0736080c
Lauri Leukkunen <lle@rahina.org>
2007-12-10 21:14:57 +0200

    Mapping rules for .deb building
    Patch by Jukka-Pekka Iivonen <jukka-pekka.iivonen@nokia.com>
    
    Adds correct mappings for many debian packaging tools and other
    build utilities.
    
    Changes emulation mode slightly to allow access to $HOME to better
    support installation of .debs or running "make install".
    
    Signed-off-by: Lauri Leukkunen <lle@rahina.org>

--- lua_scripts/pathmaps/emulate/00_default.lua
+++ lua_scripts/pathmaps/emulate/00_default.lua
@@ -12,6 +12,13 @@ mapall_chain = {
 		{path = "^/proc", map_to = nil},
 		{path = "^/tmp", map_to = nil},
 		{path = "^/sys", map_to = nil},
+-- maemo sdk hack, allows access to users' $HOME dirs
+-- to make installing from outside the buildroot possible
+-- /home/user is special on maemo systems... just don't ask, OK?
+		{path = "^/home/user.*", map_to = "="},
+		{path = "^/usr/share/scratchbox2.*", map_to = nil},
+		{path = "^/home.*", map_to = nil},
+
 		{path = ".*", map_to = "="}
 	}
 }

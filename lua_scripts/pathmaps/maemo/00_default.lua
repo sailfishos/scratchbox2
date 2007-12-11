885b0d1c95d2547f2079a49be210f580bce115e7
Lauri Leukkunen <lle@rahina.org>
2007-12-11 15:38:27 +0200

    tool distro support for maemo mapping mode
    MAEMO_TOOLS environment variable can be used to point sb2 to use
    tools from some arbitrary directory instead of the root of the host
    system. Useful for controlling the build environment.
    
    Signed-off-by: Lauri Leukkunen <lle@rahina.org>

--- lua_scripts/pathmaps/maemo/00_default.lua
+++ lua_scripts/pathmaps/maemo/00_default.lua
@@ -1,6 +1,8 @@
 -- Copyright (C) 2007 Lauri Leukkunen <lle@rahina.org>
 -- Licensed under MIT license.
 
+-- if tool_root ends up being nil, that's ok
+tool_root = os.getenv("MAEMO_TOOLS")
 
 simple_chain = {
 	next_chain = nil,
@@ -23,15 +25,15 @@ simple_chain = {
 		{path = "^/usr/share/defoma.*", map_to = "="},
 		{path = "^/usr/share/dict.*", map_to = "="},
 		{path = "^/usr/share/dpkg.*", map_to = "="},
-		{path = "^/usr/share/docbook.utils.*", map_to = nil},
+		{path = "^/usr/share/docbook.utils.*", map_to = tool_root},
 		{path = "^/usr/share/doc.*", map_to = "="},
 --trippletable error if taken from target: {path = "^/usr/share/dpkg.*", map_to = "="},
 		{path = "^/usr/share/fonts.*", map_to = "="},
 		{path = "^/usr/share/glib.*", map_to = "="},
 		{path = "^/usr/share/gnupg.*", map_to = "="},
-		{path = "^/usr/share/gtk.doc/data/gtkdoc.common*", map_to = nil},
-		{path = "^/usr/share/gtk.doc/data/gtk.doc.make*", map_to = nil},
-		{path = "^/usr/share/gtk.doc/data/.*xsl", map_to = nil},
+		{path = "^/usr/share/gtk.doc/data/gtkdoc.common*", map_to = tool_root},
+		{path = "^/usr/share/gtk.doc/data/gtk.doc.make*", map_to = tool_root},
+		{path = "^/usr/share/gtk.doc/data/.*xsl", map_to = tool_root},
 		{path = "^/usr/share/gtk.doc.*", map_to = "="},
 --gtk-doc.make error if taken from target: {path = "^/usr/share/gtk.*", map_to = "="},
 		{path = "^/usr/share/i18n.*", map_to = "="},
@@ -62,16 +64,16 @@ simple_chain = {
 		{path = "^/usr/bin/xml2.conf.*", map_to = "="},
 --glib-mkenums error: points to /scratchbox/tools/bin/perl {path = "^/usr/bin/glib.*", map_to = "="},
 		{path = "^/usr/bin/gobject.query.*", map_to = "="},
-		{path = "^/usr/lib/perl.*", map_to = nil},
-		{path = "^/usr/lib/dpkg.*", map_to = nil},
-		{path = "^/usr/lib/cdbs.*", map_to = nil},
+		{path = "^/usr/lib/perl.*", map_to = tool_root},
+		{path = "^/usr/lib/dpkg.*", map_to = tool_root},
+		{path = "^/usr/lib/cdbs.*", map_to = tool_root},
 		{path = "^/usr/lib.*", map_to = "="},
 		{path = "^/usr/include.*", map_to = "="},
 		{path = "^/usr/share/aclocal.*", map_to = "="},
 		{path = "^/var/.*/apt.*", map_to = "="},
 		{path = "^/var/.*/dpkg.*", map_to = "="},
 		{path = "^/host_usr", map_to = "="},
-		{path = ".*", map_to = nil}
+		{path = ".*", map_to = tool_root}
 	}
 }
 
@@ -82,7 +84,7 @@ qemu_chain = {
 		{path = "^/lib.*", map_to = "="},
 		{path = "^/usr/lib.*", map_to = "="},
 		{path = "^/usr/local/lib.*", map_to = "="},
-		{path = ".*", map_to = nil}
+		{path = ".*", map_to = tool_root}
 	}
 }
 
@@ -99,10 +101,10 @@ apt_chain = {
 	next_chain = simple_chain,
 	binary = ".*apt.*",
 	rules = {
-		{path = "^" .. escape_string(target_root) .. ".*", map_to = nil},
+		{path = "^" .. escape_string(target_root) .. ".*", map_to = tool_root},
 		{path = "^/var/lib/apt.*", map_to = "="},
 		{path = "^/var/cache/apt.*", map_to = "="},
-		{path = "^/usr/lib/apt.*", map_to = nil},
+		{path = "^/usr/lib/apt.*", map_to = tool_root},
 		{path = "^/etc/apt.*", map_to = "="}
 	}
 }
@@ -114,7 +116,7 @@ sh_chain = {
 	binary = ".*sh.*",
 	rules = {
 		{path = "^/usr/lib.*la", map_to = "="},
-		{path = "^/usr/lib.*", map_to = nil},
+		{path = "^/usr/lib.*", map_to = tool_root},
 	}
 }
 

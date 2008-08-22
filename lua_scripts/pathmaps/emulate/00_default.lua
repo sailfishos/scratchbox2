d9e32b3fca3b60513757934242401f12cf609100
Lauri Aarnio <Lauri.Aarnio@iki.fi>
2008-08-22 15:32:33 +0300

    "emulate" mode: Added scratchbox 1 compatibility rules (these make it possible to e.g. start application framework startup, which was possible with the old scratchbox 1, too)

--- lua_scripts/pathmaps/emulate/00_default.lua
+++ lua_scripts/pathmaps/emulate/00_default.lua
@@ -1,12 +1,23 @@
 -- Copyright (C) 2007 Lauri Leukkunen <lle@rahina.org>
 -- Licensed under MIT license.
 
+sb1_compat_dir = os.getenv("SBOX_TARGET_ROOT") .. "/scratchbox1-compat"
+
 mapall_chain = {
 	next_chain = nil,
 	binary = nil,
 	rules = {
 		{match = ".*qemu.*", use_orig_path = true},
 		{prefix = target_root, use_orig_path = true},
+
+		-- Scratchbox 1 compatibility rules:
+		{ prefix = "/targets/", map_to = sb1_compat_dir },
+		{ path = "/usr/bin/scratchbox-launcher.sh",
+                    map_to = sb1_compat_dir },
+                { path = "/etc/osso-af-init/dbus-systembus.sh",
+                    map_to = sb1_compat_dir },
+		
+		-- 
 		{prefix = "/dev", use_orig_path = true},
 		{prefix = "/proc", use_orig_path = true},
 		{prefix = "/tmp", use_orig_path = true},

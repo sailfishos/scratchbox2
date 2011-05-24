fdcc2173e744bca6fd2efbed5a297709c49fe9b3
Lauri Aarnio <Lauri.Aarnio@iki.fi>
2011-05-24 19:34:10 +0300

    pathmapping_interf.c: runtime selection of mapping engine (C,Lua or both)

--- pathmapping/pathmapping_interf.c
+++ pathmapping/pathmapping_interf.c
@@ -53,7 +53,132 @@
 
 #include "pathmapping.h" /* get private definitions of this subsystem */
 
+#define MAPPING_METHOD_NOT_SET	 	0
+#define MAPPING_METHOD_C_ENGINE	 	01
+#define MAPPING_METHOD_LUA_ENGINE	02
+#define MAPPING_METHOD_BOTH_ENGINES	(MAPPING_METHOD_C_ENGINE|MAPPING_METHOD_LUA_ENGINE)
+
+static int mapping_method = MAPPING_METHOD_NOT_SET;
+
+static void check_mapping_method(void)
+{
+	if (mapping_method == MAPPING_METHOD_NOT_SET) {
+		const char *mp;
+		mp = getenv("SBOX_MAPPING_METHOD");
+
+		if (mp) {
+			if (strchr(mp,'c')|| strchr(mp,'C')) {
+				mapping_method |= MAPPING_METHOD_C_ENGINE;
+				SB_LOG(SB_LOGLEVEL_INFO,
+					"Activated 'C' mapping method");
+			}
+			if (strchr(mp,'l')|| strchr(mp,'L')) {
+				mapping_method |= MAPPING_METHOD_LUA_ENGINE;
+				SB_LOG(SB_LOGLEVEL_INFO,
+					"Activated 'Lua' mapping method");
+			}
+			if (mapping_method == MAPPING_METHOD_NOT_SET) {
+				/* default to both */
+				mapping_method = MAPPING_METHOD_BOTH_ENGINES;
+				SB_LOG(SB_LOGLEVEL_INFO,
+					"Error in SBOX_MAPPING_METHOD variable, using both mapping methods");
+			}
+		} else {
+			/* default to both */
+			mapping_method = MAPPING_METHOD_BOTH_ENGINES;
+			SB_LOG(SB_LOGLEVEL_INFO,
+				"Activated both mapping methods");
+		}
+	}
+}
+
+static void compare_results_from_c_and_lua_engines(
+	const char *fn_name,
+	const char *c_res,
+	const char *lua_res)
+{
+	if (c_res && lua_res) {
+		if (!strcmp(c_res, lua_res)) {
+			SB_LOG(SB_LOGLEVEL_DEBUG,
+				"%s: ResultCheck: same, OK",
+				fn_name);
+		} else {
+			SB_LOG(SB_LOGLEVEL_DEBUG,
+				"%s: ResultCheck: DIFFERENT: C='%s', Lua='%s'",
+				fn_name, c_res, lua_res);
+		}
+	} else {
+		if (!c_res) {
+			SB_LOG(SB_LOGLEVEL_DEBUG,
+				"%s: ResultCheck: no result from C (Lua='%s')",
+				fn_name, lua_res);
+		}
+		if (!lua_res) {
+			SB_LOG(SB_LOGLEVEL_DEBUG,
+				"%s: ResultCheck: no result from Lua (C='%s')",
+				fn_name, c_res);
+		}
+	}
+}
+
 /* ========== Public interfaces to the mapping & resolution code: ========== */
+
+void fwd_map_path(
+	const char *binary_name,
+	const char *func_name,
+	const char *virtual_path,
+	int dont_resolve_final_symlink,
+	int exec_mode,
+	mapping_results_t *res)
+{
+	if (!virtual_path) {
+		res->mres_result_buf = res->mres_result_path = NULL;
+		res->mres_readonly = 1;
+	} else {
+		mapping_results_t res2;
+
+		check_mapping_method();
+
+		switch (mapping_method) {
+		case MAPPING_METHOD_C_ENGINE:
+			if (sbox_map_path_internal__c_engine(binary_name,
+				func_name, virtual_path,
+				dont_resolve_final_symlink, 0, res) < 0) {
+				SB_LOG(SB_LOGLEVEL_DEBUG,
+					"C path mapping engine failed, fallback to Lua (%s)",
+					virtual_path);
+				sbox_map_path_internal__lua_engine(binary_name,
+					func_name, virtual_path,
+					dont_resolve_final_symlink, 0, res);
+			}
+			return;
+		case MAPPING_METHOD_LUA_ENGINE:
+			sbox_map_path_internal__lua_engine(binary_name, func_name, virtual_path,
+				dont_resolve_final_symlink, 0, res);
+			return;
+		case MAPPING_METHOD_BOTH_ENGINES:
+			clear_mapping_results_struct(&res2);
+			sbox_map_path_internal__lua_engine(binary_name, func_name, virtual_path,
+				dont_resolve_final_symlink, 0, res);
+			if (sbox_map_path_internal__c_engine(binary_name, func_name, virtual_path,
+				dont_resolve_final_symlink, 0, &res2) < 0) {
+				SB_LOG(SB_LOGLEVEL_DEBUG,
+					"C path mapping engine failed, can't compare (%s)",
+					virtual_path);
+			} else {
+				compare_results_from_c_and_lua_engines(
+					__func__, res2.mres_result_path,
+					res->mres_result_path);
+			}
+			free_mapping_results(&res2);
+			return;
+		default:
+			SB_LOG(SB_LOGLEVEL_ERROR,
+				"%s: Invalid mapping method",
+				__func__);
+		}
+	}
+}
 
 void sbox_map_path_for_sb2show(
 	const char *binary_name,
@@ -61,13 +186,8 @@ void sbox_map_path_for_sb2show(
 	const char *virtual_path,
 	mapping_results_t *res)
 {
-	if (!virtual_path) {
-		res->mres_result_buf = res->mres_result_path = NULL;
-		res->mres_readonly = 1;
-	} else {
-		sbox_map_path_internal(binary_name, func_name, virtual_path,
-			0/*dont_resolve_final_symlink*/, 0, res);
-	}
+	fwd_map_path(binary_name, func_name, virtual_path,
+		0/*dont_resolve_final_symlink*/, 0/*exec_mode*/, res);
 }
 
 void sbox_map_path(
@@ -76,15 +196,12 @@ void sbox_map_path(
 	int dont_resolve_final_symlink,
 	mapping_results_t *res)
 {
-	if (!virtual_path) {
-		res->mres_result_buf = res->mres_result_path = NULL;
-		res->mres_readonly = 1;
-	} else {
-		sbox_map_path_internal(
-			(sbox_binary_name ? sbox_binary_name : "UNKNOWN"),
-			func_name, virtual_path, dont_resolve_final_symlink, 0, res);
-	}
+	fwd_map_path(
+		(sbox_binary_name ? sbox_binary_name : "UNKNOWN"),
+		func_name, virtual_path,
+		dont_resolve_final_symlink, 0/*exec_mode*/, res);
 }
+
 
 void sbox_map_path_at(
 	const char *func_name,
@@ -107,10 +224,10 @@ void sbox_map_path_at(
 #endif
 	   ) {
 		/* same as sbox_map_path() */
-		sbox_map_path_internal(
+		fwd_map_path(
 			(sbox_binary_name ? sbox_binary_name : "UNKNOWN"),
-			func_name,
-			virtual_path, dont_resolve_final_symlink, 0, res);
+			func_name, virtual_path,
+			dont_resolve_final_symlink, 0/*exec_mode*/, res);
 		return;
 	}
 
@@ -129,10 +246,10 @@ void sbox_map_path_at(
 			"Synthetic path for %s(%d,'%s') => '%s'",
 			func_name, dirfd, virtual_path, virtual_abs_path_at_fd);
 
-		sbox_map_path_internal(
+		fwd_map_path(
 			(sbox_binary_name ? sbox_binary_name : "UNKNOWN"),
 			func_name,
-			virtual_abs_path_at_fd, dont_resolve_final_symlink, 0, res);
+			virtual_abs_path_at_fd, dont_resolve_final_symlink, 0/*exec_mode*/, res);
 		free(virtual_abs_path_at_fd);
 
 		return;
@@ -155,8 +272,9 @@ void sbox_map_path_for_exec(
 	const char *virtual_path,
 	mapping_results_t *res)
 {
-	sbox_map_path_internal(
-		(sbox_binary_name ? sbox_binary_name : "UNKNOWN"), func_name,
+	fwd_map_path(
+		(sbox_binary_name ? sbox_binary_name : "UNKNOWN"),
+		func_name,
 		virtual_path, 0/*dont_resolve_final_symlink*/, 1/*exec mode*/, res);
 }
 

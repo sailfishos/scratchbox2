a3b6d187a58adb509aff8f133cfa499314cc69f8
Lauri Aarnio <Lauri.Aarnio@iki.fi>
2011-11-08 16:00:20 +0200

    "emulate" mode: minor fixes

--- modes/emulate/exec_rules.lua
+++ modes/emulate/exec_rules.lua
@@ -14,8 +14,8 @@ end
 
 -- Exec policy rules.
 
-default_exec_policy = {
-	name = "Default",
+exec_policy_host = {
+	name = "Host",
 	native_app_ld_preload_prefix = host_ld_preload..fakeroot_ld_preload,
 }
 
@@ -126,7 +126,7 @@ exec_policy_rules = {
 		{prefix = tools_root, exec_policy_name = "Tools"},
 
                 -- the home directory is expected to contain target binaries:
-                {prefix = sbox_user_home_dir, exec_policy_name = "Target"},
+                {dir = sbox_user_home_dir, exec_policy_name = "Target"},
 
 		-- Target binaries:
 		{prefix = target_root, exec_policy_name = "Target"},
@@ -136,7 +136,7 @@ exec_policy_rules = {
 		{prefix = sbox_workdir, exec_policy_name = "Target"},
 
 		-- DEFAULT RULE (must exist):
-		{prefix = "/", exec_policy_name = "Default"}
+		{prefix = "/", exec_policy_name = "Host"}
 }
 
 -- This table lists all exec policies - this is used when the current
@@ -144,6 +144,6 @@ exec_policy_rules = {
 all_exec_policies = {
 	exec_policy_target,
 	exec_policy_tools,
-	default_exec_policy,
+	exec_policy_host,
 }
 

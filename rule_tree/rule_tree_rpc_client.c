94174849e24e1e5f600aeb79df1ec1739d0bb6fd
Lauri Aarnio <Lauri.Aarnio@iki.fi>
2012-02-17 19:44:33 +0200

    Added client-side RPC routines for communicating with sb2d
    - and utils/sb2-ruletree-client.c, a simple tool for
      testing the connection

---
+++ rule_tree/rule_tree_rpc_client.c
@@ -0,0 +1,265 @@
+/*
+ * Copyright (C) 2011 Nokia Corporation.
+ * Author: Lauri T. Aarnio
+ *
+ * Licensed under LGPL version 2.1, see top level LICENSE file for details.
+*/
+
+/* Rule tree client-side library:
+ * To be used from inside an active SB2 session for
+ * connecting to sb2d, the rule tree db server process.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include "mapping.h"
+#include "sb2.h"
+#include "libsb2.h"
+#include "rule_tree.h"
+#include "rule_tree_rpc.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/vfs.h>
+#include <sys/statvfs.h>
+
+#include "exported.h"
+
+static struct sockaddr_un server_address;
+static socklen_t	server_addr_len;
+static struct sockaddr_un client_address;
+static char *client_socket_path = NULL;
+
+static int client_socket = -1;
+static int server_address_initialized = 0;
+
+static int initialize_server_address(void)
+{
+	char	*sock_path = NULL;
+	size_t	sock_path_len;
+
+	if (server_address_initialized) return(0);
+
+	if (!sbox_session_dir) {
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: session directory is missing.");
+		return(-1);
+	}
+	if (asprintf(&sock_path, "%s/sb2d-sock.d/ssock", sbox_session_dir) < 0) {
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: asprintf failed");
+		return(-1);
+	}
+	sock_path_len = strlen(sock_path);
+	if (sock_path_len >= sizeof(server_address.sun_path)-1) {
+		/* This should never happen, server should not start
+		 * if address would be too long
+		 * (and session creation should fail), but check anyways */
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: server socket address is too long (%s)",
+			sock_path);
+		return(-1);
+	}
+	server_address.sun_family = AF_UNIX;
+	strcpy(server_address.sun_path, sock_path); /* it fits. */
+	server_addr_len = sizeof(sa_family_t) + sock_path_len + 1;
+	free(sock_path);
+
+	server_address_initialized = 1;
+	return(0);
+}
+
+static void cleanup_client_socket(void)
+{
+	SB_LOG(SB_LOGLEVEL_DEBUG, "ruletree_rpc: cleanup");
+	if (client_socket >= 0) {
+		close(client_socket);
+		client_socket = -1;
+	}
+	if (client_socket_path) {
+		unlink_nomap_nolog(client_socket_path);
+		free(client_socket_path);
+		client_socket_path = NULL;
+	}
+}
+
+static int create_client_socket(void)
+{
+	socklen_t	client_addr_len;
+	size_t		sock_path_len;
+	int		min_fd;
+
+	client_socket = socket(PF_UNIX, SOCK_DGRAM, 0);
+	if (client_socket < 0) {
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: Failed to create client socket");
+		goto error_out;
+	}
+	min_fd = ruletree_get_min_client_socket_fd();
+	if (client_socket < min_fd) {
+		/* find lowest free fd above min_fd */
+		int new_fd = fcntl(client_socket, F_DUPFD, (long)min_fd);
+		if (new_fd < 0) {
+			SB_LOG(SB_LOGLEVEL_ERROR,
+				"ruletree_rpc: failed to move socket FD > %d",
+				min_fd);
+		} else {
+			close(client_socket);
+			client_socket = new_fd;
+			SB_LOG(SB_LOGLEVEL_NOISE,
+				"ruletree_rpc: client socket FD = %d",
+				client_socket);
+		}
+	}
+	if (asprintf(&client_socket_path, "%s/sock/%d", sbox_session_dir, getpid()) < 0) {
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: asprintf failed");
+		goto error_out;
+	}
+	sock_path_len = strlen(client_socket_path);
+	if (sock_path_len >= sizeof(client_address.sun_path)-1) {
+		/* This should never happen, server should not start
+		 * if address would be too long
+		 * (and session creation should fail), but check anyways */
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: client socket address is too long (%s)",
+			client_socket_path);
+		goto error_out;
+	}
+	client_address.sun_family = AF_UNIX;
+	strcpy(client_address.sun_path, client_socket_path); /* it fits. */
+	unlink_nomap_nolog(client_socket_path); /* remove old socket, if any. */
+
+	client_addr_len = sizeof(sa_family_t) + sock_path_len + 1;
+	if (bind_nomap_nolog(client_socket, (struct sockaddr*)&client_address, client_addr_len) < 0) {
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"ruletree_rpc: Failed to bind client socket address (%s)",
+			client_socket_path);
+		goto error_out;
+	}
+
+	SB_LOG(SB_LOGLEVEL_DEBUG,
+		"ruletree_rpc: client socket = (%s)",
+		client_socket_path);
+	/* client socket has been initialized. */
+	atexit(cleanup_client_socket);
+	return(0);
+
+   error_out:
+	cleanup_client_socket();
+	return(-1);
+}
+
+/* use a mutex to allow only one thread to access the socket, if libpthreads is available.
+ * If it isn't, this is used in a sigle-threaded program and we can
+ * safely live without the mutex.
+*/
+static pthread_mutex_t	client_socket_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static int send_command_receive_reply(
+	ruletree_rpc_msg_command_t	*command,
+	ruletree_rpc_msg_reply_t	*reply)
+{
+	ssize_t	sent_msg_size;
+	ssize_t	received_msg_size;
+	int use_locking = 0;
+
+	if (pthread_library_is_available) {
+		use_locking = 1;
+		SB_LOG(SB_LOGLEVEL_NOISE, "Going to lock client_socket_mutex");
+		(*pthread_mutex_lock_fnptr)(&client_socket_mutex);
+	}
+
+	if (server_address_initialized == 0) {
+		if (initialize_server_address() < 0) {
+			SB_LOG(SB_LOGLEVEL_ERROR,
+				"Failed to initialize server socket address (ruletree_rpc)");
+			goto error_out;
+		}
+	}
+
+    reopen_socket:
+	if (client_socket < 0) {
+		if (create_client_socket() < 0) {
+			SB_LOG(SB_LOGLEVEL_ERROR,
+				"Failed to create client socket (ruletree_rpc)");
+			goto error_out;
+		}
+	}
+
+	command->rimc_message_protocol_version = RULETREE_RPC_PROTOCOL_VERSION;
+	/* FIXME: fill serial */
+	sent_msg_size = sendto_nomap_nolog(client_socket, command, sizeof(*command), 0,
+		(struct sockaddr*)&server_address, server_addr_len);
+	if (sent_msg_size < 0) {
+		switch (errno) {
+		case ENOTSOCK:
+		case EBADF:
+			/* bad fd; assume the application has closed or reused
+			 * it; because it might be open for another use already,
+			 * we don't attempt to close it here. */
+			client_socket = -1;
+			goto reopen_socket;
+		}
+
+		SB_LOG(SB_LOGLEVEL_ERROR,
+			"Failed to send command to server (ruletree_rpc)");
+		goto error_out;
+	}
+
+	SB_LOG(SB_LOGLEVEL_DEBUG, "ruletree_rpc: sendto => %d", (int)sent_msg_size);
+		
+	received_msg_size = recvfrom_nomap_nolog(client_socket, reply, sizeof(*reply), 0,
+		(struct sockaddr*)NULL, (socklen_t*)NULL);
+	SB_LOG(SB_LOGLEVEL_DEBUG, "ruletree_rpc: recvfrom => %d", (int)received_msg_size);
+	/* FIXME: check serial */
+	/* FIXME: check sender address? */
+	if (received_msg_size <= 0) {
+		goto error_out;
+	}
+	/* FIXME: If message is too small... */
+	if (use_locking) {
+		(*pthread_mutex_unlock_fnptr)(&client_socket_mutex);
+		SB_LOG(SB_LOGLEVEL_NOISE, "unlocked client_socket_mutex");
+	}
+	SB_LOG(SB_LOGLEVEL_DEBUG,
+		"%s: Received reply type=%u", __func__, reply->rimr_message_type);
+	return(0);
+
+    error_out:
+	if (use_locking) {
+		(*pthread_mutex_unlock_fnptr)(&client_socket_mutex);
+		SB_LOG(SB_LOGLEVEL_NOISE, "unlocked client_socket_mutex");
+	}
+	return(-1);
+}
+
+void ruletree_rpc__ping(void)
+{
+	ruletree_rpc_msg_command_t	command;
+	ruletree_rpc_msg_reply_t	reply;
+
+	SB_LOG(SB_LOGLEVEL_DEBUG,
+		"ruletree_rpc: Sending command 'ping'");
+	memset(&command, 0, sizeof(command));
+	command.rimc_message_type = RULETREE_RPC_MESSAGE_COMMAND__PING;
+	send_command_receive_reply(&command, &reply);
+}
---
+++ utils/sb2-ruletree-client.c
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2011 Nokia Corporation.
+ * Author: Lauri T. Aarnio
+ *
+ * Licensed under LGPL version 2.1, see top level LICENSE file for details.
+*/
+
+/* Rule tree client: A debugging tool, can be used to send commands to sb2d.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <assert.h>
+
+#include "sb2.h"
+#include "rule_tree.h"
+#include "rule_tree_rpc.h"
+#include "mapping.h"
+#include "libsb2.h"
+
+#include "exported.h"
+
+
+
+static const char *progname = NULL;
+char    *sbox_session_dir = NULL;
+
+int ruletree_get_min_client_socket_fd(void)
+{
+	return(10);
+}
+
+int main(int argc, char *argv[])
+{
+	int	opt;
+	const char	*cmd;
+
+	progname = argv[0];
+
+	/* init logging before doing anything else,
+	 * otherwise the logger will auto-initialize itself.
+	 * if debug_level and/or debug_file is NULL, logger
+	 * will read the values from env.vars. */
+	sblog_init_level_logfile_format("debug", "-", NULL);
+
+	while ((opt = getopt(argc, argv, "d:")) != -1) {
+		switch (opt) {
+#if 0
+		case 'd':
+			debug = sb_loglevel__ = atoi(optarg);
+			break;
+#endif
+		default:
+			fprintf(stderr, "Illegal option\n");
+			exit(1);
+		}
+	}
+
+	sbox_session_dir = getenv("SBOX_SESSION_DIR");
+	if (!sbox_session_dir) {
+		fprintf(stderr, "ERROR: no session "
+			"(SBOX_SESSION_DIR is not set) - this program"
+			" must be used inside a Scratchbox 2 session\n");
+		exit(1);
+	}
+
+	cmd = argv[optind];
+	if (!cmd) {
+		fprintf(stderr, "Usage:\n\t%s command\n", argv[0]);
+		fprintf(stderr, "commands\n"
+				"   ping     Send a 'ping' to sb2d, wait for reply\n");
+		exit(1);
+	}
+
+	if (!strcmp(cmd, "ping")) {
+		ruletree_rpc__ping();
+	} else {
+		fprintf(stderr, "Unknown command %s\n", cmd);
+		exit(1);
+	}
+
+	return(0);
+}
+
+/* This program is directly linked to the RPC routines
+ * (because they are hidden in libsb2, and cound not be used otherwise).
+ * The RPC routines want some wrappers for *_nomap_nolog functions,
+ * these will use the ordinary functions... a side-effect is that the 
+ * network addresses are subject to mapping operations here. 
+ * Fortunately that won't happen in the usual case when the RPC
+ * functions are used inside libsb2.
+*/
+int unlink_nomap_nolog(const char *pathname)
+{
+	return(unlink(pathname));
+}
+
+ssize_t sendto_nomap_nolog(int s, const void *buf, size_t len, int flags, const struct sockaddr *to, socklen_t tolen)
+{
+	return(sendto(s, buf, len, flags, to, tolen));
+}
+
+ssize_t recvfrom_nomap_nolog(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen)
+{
+	return(recvfrom(s, buf, len, flags, from, fromlen));
+}
+
+int bind_nomap_nolog(int sockfd, const struct sockaddr *my_addr, socklen_t addrlen)
+{
+	return(bind(sockfd, my_addr, addrlen));
+}

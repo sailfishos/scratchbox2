8154f885f557e75a229710920489480ca2e7d454
David Greaves <david@dgreaves.com>
2013-04-25 10:38:08 +0000

    accel-ccache

--- modes/obs-rpm-build/fs_rules.lua
+++ modes/obs-rpm-build/fs_rules.lua
@@ -365,6 +365,9 @@ emulate_mode_rules_usr_bin = {
 		{path="/usr/bin/localedef",
 		 func_class = FUNC_CLASS_EXEC,
 		 actions=accelerated_program_actions},
+		{path="/usr/bin/ccache",
+		 func_class = FUNC_CLASS_EXEC,
+		 actions=accelerated_program_actions},
 
 		{path = "/usr/bin/sb2-show", use_orig_path = true,
 		 protection = readonly_fs_always},

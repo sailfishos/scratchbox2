dc0a001ff94bee1fe3ef0eef814bda030d5b31ef
Oleg Girko <ol@infoserver.lv>
2015-09-18 01:15:05 +0100

    Add special treatment for plugins in "/usr/lib" to modes.
    Now all shared libraries in "/usr/lib" that are opened with dlopen()
    by programs from tools ("Tools" exec class) are searched in
    tool root first.
    
    Otherwise, a program from tools (that has host architecture)
    will be unable to load plugin from target root
    (that has target architecture).
    
    Also, plugins are searched in "/usr/lib64" first,
    in case of 64-bit host system that has shared libraries in "/usr/lib64"
    and 32-bit target that has shared libraries in "/usr/lib".
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- modes/obs-rpm-build+pp/fs_rules.lua
+++ modes/obs-rpm-build+pp/fs_rules.lua
@@ -618,6 +618,21 @@ emulate_mode_rules_usr = {
 
 		{dir = "/usr/share", rules = usr_share_rules},
 
+		-- If a program from tools loads plugins,
+		-- they should be dlopened from tools as well.
+		-- However, libdir in tools can be different than one in target.
+		{dir = "/usr/lib", func_class = FUNC_CLASS_DLOPEN,
+		 actions = {
+		  {if_active_exec_policy_is = "Tools",
+		   if_exists_then_replace_by = tools .. "/usr/lib64",
+		   protection = readonly_fs_always},
+		  {if_active_exec_policy_is = "Tools",
+		   if_exists_then_map_to = tools,
+		   protection = readonly_fs_always},
+		  { map_to = target_root, protection = readonly_fs_always },
+		 },
+		},
+
 		{dir = "/usr", map_to = target_root,
 		protection = readonly_fs_if_not_root}
 }

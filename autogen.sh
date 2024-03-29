090a710bba62d0b0c4fb4bca2560fe97b145874d
Björn Bidar <bjorn.bidar@jolla.com>
2024-03-29 03:19:35 +0200

    [sb2] Use autotools builtin system to rerun configure instead of our own
    By detecting if configure.ac has changed and thus configure will change
    in the following step we can run autogen.sh and config.status to rerun
    configure with the previously set built options.
    
    The change removes the need for .configure file, correctly detects
    if configures sources changed and doesn't run configure again without
    all the previously passed options.
    
    Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-#automake_version=1.9
+# Stale caches can confuse autoconf.
+rm -fr autom4te.cache || exit
 
-rm -rf configure autom4te.cache
+# Let autoreconf figure out what, if anything, needs doing.
+# Use autoreconf's -f option in case autoreconf itself has changed.
 autoreconf --verbose --force --install

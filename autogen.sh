8d565338b189d9ddf964d41e535bcdfeb8129765
Björn Bidar <bjorn.bidar@jolla.com>
2024-11-25 02:14:30 +0200

    [sb2] Avoid rebuilds when configure.ac changes but not it's output
    When configure.ac or config.mak.in change but not their output their
    will be repeated rebuilds as the timestamp their output files such as
    config.mak stay older than their dependency.
    
    The timestamp file's timestamp will always update after the targets
    dependency changed.
    
    Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>

--- autogen.sh
+++ autogen.sh
@@ -6,3 +6,8 @@ rm -fr autom4te.cache || exit
 # Let autoreconf figure out what, if anything, needs doing.
 # Use autoreconf's -f option in case autoreconf itself has changed.
 autoreconf --verbose --force --install
+
+# Create a timestamp, so that './autogen.sh; make' doesn't
+# cause 'make' to needlessly run 'autoheader'.
+echo > stamp-h.in
+echo > stamp-mak.in
